4fc13124a61f9199a5b517c324c8f6fdc98695f2
GamerZ <GamerZ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-02 18:40:06 +0000

    a
    git-svn-id: https://plugins.svn.wordpress.org/wp-wap/trunk@307055 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-wap.php
+++ wp-wap.php
@@ -25,5 +25,7 @@ Author URI: http://lesterchan.net
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+boobs
 */
 ?>
